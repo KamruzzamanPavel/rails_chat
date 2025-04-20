3c47bca212307f5d470005af8d194ecc6cbb89e5
KamruzzamanPavel <kamruzzamanpavel88@gmail.com>
2025-04-21 01:15:26 +0600

    ok

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,5 +3,6 @@ class Message < ApplicationRecord
   belongs_to :room
   validates :content, presence: true
 
-  after_create_commit { broadcast_append_to room }
+  
+  after_create_commit { broadcast_append_to room, target: "messages" }
 end
